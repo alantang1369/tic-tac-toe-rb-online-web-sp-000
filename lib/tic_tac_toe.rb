ebd3ddf867c38f063a4822e810119a097d891120
Alan Tang <giabao_tang@yahoo.com>
2020-09-19 23:39:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,8 +10,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
+def move(board, index, current_player(board))
+  board[index] = current_player(board)
 end
 
 def position_taken?(board, location)
@@ -23,7 +23,7 @@ def valid_move?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
+  puts "Player #{current_player(board)}, please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
@@ -41,4 +41,18 @@ def play(board)
     turn(board)
     n += 1 
   end  
+end  
+
+def turn_count(board)
+  count = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X":"O"
 end  
