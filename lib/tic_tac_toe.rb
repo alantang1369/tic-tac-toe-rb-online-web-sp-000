238b8f3e3901dc23c2763a7618849913810e6e8a
Alan Tang <giabao_tang@yahoo.com>
2020-09-19 23:44:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,9 +14,7 @@ def move(board, index, current_player(board))
   board[index] = current_player(board)
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
+
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
@@ -37,10 +35,11 @@ end
 # Define your play method below
 def play(board)
   
-  until 
+  until over?(board)
     turn(board)
-    n += 1 
-  end  
+    
+  end 
+  winner(board)
 end  
 
 def turn_count(board)
@@ -55,4 +54,54 @@ end
 
 def current_player(board)
   turn_count(board) % 2 == 0? "X":"O"
+end  
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+  ]
+  
+def won?(board)
+  to_a = []
+  if WIN_COMBINATIONS.any? do |win_index|
+   if  win_index.all? { |index| board[index] == "X" } || win_index.all? { |index| board[index] == "O" }
+      to_a = win_index
+   end
+     end
+  to_a
+  end
+  
+end  
+
+def full?(board)
+  if board.all? { |i| i != " "}
+    true
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  end
+end  
+    
+def over?(board)
+  if draw?(board) || won?(board)
+    true
+  end
+end  
+
+def winner(board)
+  if won?(board)
+     win_token = won?(board)
+     if board[win_token[0]] == "X"
+       "X"
+     else 
+       "O"
+     end
+  end   
 end  
