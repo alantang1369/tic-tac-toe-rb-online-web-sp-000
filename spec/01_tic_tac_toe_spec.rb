24925131a4920c0459cb4819713a2ceafe36cda9
Alan Tang <giabao_tang@yahoo.com>
2020-09-20 00:03:10 +0000

    Automatically backed up by Learn

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -75,7 +75,7 @@ describe './lib/tic_tac_toe.rb' do
 
     it 'allows "X" player in the bottom right and "O" in the top left ' do
       board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-      move(board, 0, 'O')
+      move(board, 0, 'X')
       move(board, 8, 'X')
       expect(board).to eq(['O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'])
     end
